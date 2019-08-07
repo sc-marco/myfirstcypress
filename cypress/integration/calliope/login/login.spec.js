ce09d22d8de4a2b32b13d356a7318bce69336a37
sc-marco <marco.gorter@spritecloud.com>
2019-08-07 12:05:22 +0200

    password change

--- cypress/integration/calliope/login/login.spec.js
+++ cypress/integration/calliope/login/login.spec.js
@@ -1,12 +1,16 @@
 describe('First test Calliope' , () => {
+    const username = 'marco.gorter@spritecloud.com'
+    const password = 'Te$tAutomagician'
+
     it('Navigate to Calliope' , () => {
         cy.visit('https://app.calliope.pro/login')
     })
 
     it('Login to application', () => {
-        cy.get('#user_session_email').type('marco.gorter@spritecloud.com')
-        cy.get('#user_session_password').type('nywa4evtu+kEatva')
-        cy.wait(5000)
-        cy.get(':nth-child(8) > .btn-primary').click()
+        // cy.get('#user_session_email').type('marco.gorter@spritecloud.com')
+        // cy.get('#user_session_password').type('Te$tAutomagician')
+        // cy.wait(5000)
+        // cy.get(':nth-child(8) > .btn-primary').click()
+        
     })
 })
